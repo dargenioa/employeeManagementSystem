889bab0cf0dc14f0b1b59162f50b6a44ac3a6d7a
Amanda D'Argenio <dargenioa14@gmail.com>
2020-11-23 20:13:25 -0500

    added inquirer choices to start and included switch statement based on case. Returning data that needs to be sorted and formatted

--- server.js
+++ server.js
@@ -28,11 +28,62 @@ connection.connect((err) => {
 });
 
 const startApplication = () => {
-  connection.query("SELECT * FROM role", function (err, res) {
-    if (err) throw err;
-  console.log(res);
-  // console.table('role', [res]
+  inquirer
+    .prompt({
+      name: "viewAddChange",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View all employees",
+        "View employees by deparment",
+        "View employees by job role",
+        "Add a new employee",
+        "Add a new role",
+        "Add a new department",
+        "Update an existing employee"]
+    })
+    .then(function (answer) {
+      let choice = answer.viewAddChange;
+      switch (choice) {
+        case "View all employees":
+          viewAll();
+          break;
 
-  // );
-});
+        //   case "View employees by deparment":
+        //     multiSearch();
+        //     break;
+
+        //   case "View employees by job role":
+        //     rangeSearch();
+        //     break;
+
+        //   case "Add a new employee":
+        //     songSearch();
+        //     break;
+
+        //   case "Add a new role":
+        //     songAndAlbumSearch();
+        //     break;
+
+        //   case "Add a new department":
+        //     songAndAlbumSearch();
+        //     break;
+
+        //   case "Update an existing employee":
+        //     songAndAlbumSearch();
+        //     break;
+        //   // }
+        // });
+      };
+    });
 };
+
+const viewAll = () => {
+  let query = "SELECT * FROM employee, department, role";
+  connection.query(query, function(err, res) {
+    console.log(res);
+
+  });
+
+}
+
